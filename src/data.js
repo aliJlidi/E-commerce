b35710918c22997db5716592cda56e6444315b25
Ali Jlidi <37947410+aliJlidi@users.noreply.github.com>
2022-10-31 23:47:28 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+ const data = {
+    products:[
+        {
+        _id:'1',
+        name:'Nike Slim Shirt',
+        category:'Shirts',
+        image:'./images/p1.jpg',
+        price:120,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        _id:'2',
+        name:'Addidas Fit Shirt',
+        category:'Shirts',
+        image:'./images/p2.jpg',
+        price:150,
+        brand:'Addidas',
+        rating:3.8,
+        numReviews:12,
+        description:'high quality product'
+    },
+    {
+        _id:'3',
+        name:'Nike Sport Pant',
+        category:'Pants',
+        image:'./images/p3.jpg',
+        price:80,
+        brand:'Nike',
+        rating:4.1,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        _id:'4',
+        name:'Air Max Shoes',
+        category:'Shoes',
+        image:'./images/p4.jpg',
+        price:220,
+        brand:'Nike',
+        rating:5,
+        numReviews:20,
+        description:'high quality product'
+    },
+    {
+        _id:'5',
+        name:'Nike Short Pant',
+        category:'Pants',
+        image:'./images/p5.jpg',
+        price:150,
+        brand:'Nike',
+        rating:3.5,
+        numReviews:50,
+        description:'high quality product'
+    },
+]
+}
+export default data ; 
